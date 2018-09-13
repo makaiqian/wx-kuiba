968c2c50d679409d8ebe9d4ea9b4183979cbc3b7
夜翎 <kaiqian.mkq@alibaba-inc.com>
2018-09-14 00:34:38 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,60 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    list: [{
+      img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_2934.jpg',
+      starNum: 0,
+      desc: '喵！我是魁拔，早安哦~又是元气满满的一天！'
+    }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_2497.JPG',
+        starNum: 0,
+        desc: '生日快乐哟~小萌子~'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_2335.JPG',
+        starNum: 0,
+        desc: '这些都是本喵滴！'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_2193.JPG',
+        starNum: 0,
+        desc: '我抱住了鞋鞋~你就不用去上班啦~陪我嘛~'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/LRG_DSC05649.JPG',
+        starNum: 0,
+        desc: '我们拉钩！你要把我照顾得好好的！'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1963.JPG',
+        starNum: 0,
+        desc: '啥时候下班回来陪我喵？'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1960.JPG',
+        starNum: 0,
+        desc: '啊呜~啊呜~好吃！再来一打！'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1856.JPG',
+        starNum: 0,
+        desc: '乖乖吃饭的魁拔~'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_4148.JPG',
+        starNum: 0,
+        desc: '呜呜~家里来了其它喵喵~'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_2453.JPG',
+        starNum: 0,
+        desc: '刁民们~快给本大爷请安！'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1628.JPG',
+        starNum: 0,
+        desc: '每日必备——萌喵撒娇~~'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1341%20%282%29.jpg',
+        starNum: 0,
+        desc: '每天早上醒来'
+      }, {
+        img: 'https://kuiba.oss-cn-beijing.aliyuncs.com/IMG_1188.PNG',
+        starNum: 0,
+        desc: '剃毛以后，总在家里的镜子看到一只丑八怪！'
+      }]
+  }
+})
