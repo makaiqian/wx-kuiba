18b6e91f4460cd7972f6e6f2f59e65b36a59d3f9
makaiqian <makaiqian@makaiqian.com>
2019-01-29 22:26:27 +0800

    防止多次点击

--- cloudfunctions/updateStar/index.js
+++ cloudfunctions/updateStar/index.js
@@ -7,7 +7,32 @@ const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const { id, starItem } = event
+  const { id, starItem, openId } = event
+  let p = new Promise((resolve, reject) => {
+    await db.collection('list_page').doc(id).get()
+    .then(res => {
+      if (res.data && res.data.starList) {
+        let isStar = false
+        res.data.starList.forEach((item, index) => {
+          if (item.id === openId) {
+            isStar = true
+            
+          }
+        })
+        if (isStar ==== true) {
+          reject()
+        } else {
+          resolve()
+        }
+      } else {
+        reject()
+      }
+    })
+    .catch(err => {
+      reject()
+    })
+  })
+  
   return await db.collection('list_page').doc(id).update({
     data: {
       starList: db.command.push(starItem)
